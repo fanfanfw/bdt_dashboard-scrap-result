5ef32025f955b96cf245c6f72dea4624f8d684d6
fanfanfw <firgiawan.fanfan@gmail.com>
2025-05-18 21:24:56 +0700

    menambahkan data listing, menghapus api lama

--- car_ads_dashboard/dashboard/urls.py
+++ car_ads_dashboard/dashboard/urls.py
@@ -4,7 +4,7 @@ from .views import trigger_sync
 
 urlpatterns = [
     path('user/<str:username>/', views.user_dashboard, name='user_dashboard'),
+    path('listing/<str:username>/', views.data_listing, name='data_listing'),
     path('admin/<str:username>/', views.admin_dashboard, name='admin_dashboard'),
     path('admin/<str:username>/sync/', trigger_sync, name='trigger_sync'),
-    path('api/price-vs-mileage/<str:username>/', views.api_price_vs_mileage, name='api_price_vs_mileage'),
 ]

06f716c4f4eaba433c6916046514ed787a46acdc
fanfanfw <fanfan.firgiawan69@gmail.com>
2025-07-16 20:54:47 +0700

    menambahkan route register, log dan approval

--- car_ads_dashboard/dashboard/urls.py
+++ car_ads_dashboard/dashboard/urls.py
@@ -3,12 +3,20 @@ from . import views
 from .views import trigger_sync
 
 urlpatterns = [
+    path('register/', views.register_user, name='register_user'),
+    path('check-username/', views.check_username, name='check_username'),
     path('user/<str:username>/', views.user_dashboard, name='user_dashboard'),
     path('listing/<str:username>/', views.user_dataListing, name='user_dataListing'),
     path('admin/<str:username>/', views.admin_dashboard, name='admin_dashboard'),
     path('admin/<str:username>/users/', views.admin_user_approval, name='admin_user_approval'),
+    path('admin/<str:username>/users/approve/', views.approve_user, name='approve_user'),
+    path('admin/<str:username>/users/reject/', views.reject_user, name='reject_user'),
+    path('admin/<str:username>/users/approve-all/', views.approve_all_users, name='approve_all_users'),
+    path('admin/<str:username>/users/details/', views.get_user_details, name='get_user_details'),
+    path('admin/<str:username>/stats/', views.admin_dashboard_stats, name='admin_dashboard_stats'),
     path('admin/<str:username>/server/', views.admin_server_monitor, name='admin_server_monitor'),
     path('admin/<str:username>/analytics/', views.admin_analytics, name='admin_analytics'),
+    path('admin/<str:username>/logs/', views.admin_logs, name='admin_logs'),
     path('admin/<str:username>/sync/', trigger_sync, name='trigger_sync'),
     path('api/server-metrics/', views.server_metrics_api, name='server_metrics_api'),
     path('get-models/', views.get_models, name='get_models'),

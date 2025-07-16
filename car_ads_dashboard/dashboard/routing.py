b1fdd7838ceff88530f5236ced248fed8545eca9
fanfanfw <fanfan.firgiawan69@gmail.com>
2025-07-16 20:54:11 +0700

    menambahkan path log consumer

--- car_ads_dashboard/dashboard/routing.py
+++ car_ads_dashboard/dashboard/routing.py
@@ -3,4 +3,5 @@ from dashboard import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/sync_notify/$', consumers.SyncNotificationConsumer.as_asgi()),
+    re_path(r'ws/logs/(?P<session_id>[^/]+)/$', consumers.LogsConsumer.as_asgi()),
 ]
